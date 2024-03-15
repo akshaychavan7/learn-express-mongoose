4ccd52c75c2db13ac6bc17014d402d5d13f93c7b
= <=>
2024-03-15 16:59:45 -0400

    fetch data from mongodb excercise

--- pages/authors.js
+++ pages/authors.js
@@ -1,7 +1,7 @@
 let Author = require('../models/author');
 
 get_author_list = async () => {
-  let authors_list = [];
+  let authors_list = await Author.find({}).exec();
   return authors_list.map(function(author) {
     return author.name + " : " + author.lifespan;
   });
@@ -10,5 +10,5 @@ get_author_list = async () => {
 exports.show_all_authors = function(res) {
   get_author_list()
     .then((data) => res.send(data))
-    .catch((_) => res.send('No authors found'));
+    .catch((e) => res.send('No authors found'));
 }
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,6 @@
 let BookInstance = require('../models/bookinstance');
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+exports.show_all_books_status = async function(res) {
+  console.log("Test", BookInstance.find({status: 'Available'}))
+  return res.send(await BookInstance.find({status: 'Available'}).exec());
 }
