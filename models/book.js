9861a3b1fc1e4df12f075b06b0e7e6c2c7ab6834
= <=>
2024-03-15 16:34:53 -0400

    update book schema

--- models/book.js
+++ models/book.js
@@ -7,7 +7,7 @@ var BookSchema = new Schema(
     title: {type: String, required: true},
     author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
     summary: {type: String, required: true},
-    ISBN: {type: String, required: true},
+    ISBN: {type: String},
     genre: [{type:Schema.Types.ObjectId, ref:'Genre', required: false}]
   }
 );
