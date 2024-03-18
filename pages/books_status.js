7035129c3301cbf6bbe86e16b2b9bbf0498ca682
= <=>
2024-03-18 18:44:02 -0400

    update available books API

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,6 +1,11 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = async function(res) {
-  console.log("Test", BookInstance.find({status: 'Available'}))
-  return res.send(await BookInstance.find({status: 'Available'}).exec());
+  await BookInstance.find({status: 'Available'})
+  .populate('book')
+  .exec()
+  .then(list_bookinstances=> {
+    res.send(list_bookinstances.map((bookInstance)=>bookInstance.book.title + " : "+ bookInstance.status))
+  })
+  .catch(err=> res.send('Status not found'))
 }
